058867d052f32381ef625cf22b9d7257624f7505
JHale3 <learnspark3@gmail.com>
2024-01-29 17:30:57 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function addList(args) {
+    let sum = 0;
+    for (let i = 0; i < args.length; i++) {
+      sum += args[i];
+    }
+    return sum;
+  }
+  console.log(addList([1, 2, 3]);
+  
+  
